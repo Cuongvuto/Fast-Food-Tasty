c6b57cefaa1304a91d4a4b4c700a9d39c6dd7f46
Cuongvuto <anhdeptraic@gmail.com>
2025-12-11 19:36:42 +0700

    moi2

--- fastfood-backend/middlewares/uploadCloud.js
+++ fastfood-backend/middlewares/uploadCloud.js
@@ -2,6 +2,13 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
+console.log("-----------------------------------------");
+console.log("🕵️ KIỂM TRA KẾT NỐI CLOUDINARY (TRÊN SERVER):");
+console.log("- CLOUDINARY_NAME:", process.env.CLOUDINARY_NAME ? "✅ Đã nhận" : "❌ KHÔNG TÌM THẤY (NULL)");
+console.log("- CLOUDINARY_KEY:", process.env.CLOUDINARY_KEY ? "✅ Đã nhận" : "❌ KHÔNG TÌM THẤY (NULL)");
+console.log("- CLOUDINARY_SECRET:", process.env.CLOUDINARY_SECRET ? "✅ Đã nhận" : "❌ KHÔNG TÌM THẤY (NULL)");
+console.log("-----------------------------------------");
+
 // Cấu hình Cloudinary (lấy từ .env)
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
--- fastfood-backend/server.js
+++ fastfood-backend/server.js
@@ -1,8 +1,16 @@
 // server.js
+
+// 👇 1. Thêm đoạn này lên ĐẦU file
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server đang chạy tại http://localhost:${PORT}`);
+    // 👇 2. Thêm dòng này để biết Server đang chạy ở mode nào
+    console.log(`MODE: ${process.env.NODE_ENV || 'development'}`);
 });
