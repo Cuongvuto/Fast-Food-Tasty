64e86da91c45e8f0b7772c3ac49f2908010ef4a5
Cuongvuto <anhdeptraic@gmail.com>
2025-12-11 19:09:19 +0700

    moi

--- fastfood-frontend/src/API/axiosConfig.js
+++ fastfood-frontend/src/API/axiosConfig.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    // headers: {
+    //     'Content-Type': 'application/json',
+    // },
 
     withCredentials: true
 });
